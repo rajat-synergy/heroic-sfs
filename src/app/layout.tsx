25c6a8bbcda25a8863845c6c98c5d486e35ad36b
Rajat Verma <rajat@sfswebtech.com>
2024-06-11 11:30:41 +0530

    fix styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,6 @@ export default function RootLayout({ children, }: { children: React.ReactNode; }
 
   return (
     <html suppressHydrationWarning={true} className="!scroll-smooth" lang="en">
-
       <body>
         {loading ? (<PreLoader />) : (
           <>
--- src/components/Common/PreLoader.tsx
+++ src/components/Common/PreLoader.tsx
@@ -2,10 +2,9 @@ import React from "react";
 
 const PreLoader = () => {
   return (
-    <div></div>
-    // <div className="fixed left-0 top-0 z-999999 flex h-screen w-screen items-center justify-center bg-white">
-    //   <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div>
-    // </div>
+    <div className="fixed left-0 top-0 z-999999 flex h-screen w-screen items-center justify-center bg-black">
+      <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid loding__border border-t-transparent"></div>
+    </div>
   );
 };
 
