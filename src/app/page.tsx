c4da642e86c593fdac9e35906608e642e13aba2e
Rajat Verma <rajat@sfswebtech.com>
2024-06-13 18:58:10 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import About from "@/components/About/About";
 
 export default function Home() {
   return (
-   <main>
-    
-   </main>
+    <main>
+      <About />
+    </main>
   );
 }
