65137c385706db73a36804aeadc47ed96d959831
Rajat Verma <rajat@sfswebtech.com>
2024-06-14 14:02:19 +0530

    Updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // { path: 'about', redirectTo: 'about', pathMatch: 'full'},
+    { path: 'about', component: AboutComponent, pathMatch: 'full' },
+];
